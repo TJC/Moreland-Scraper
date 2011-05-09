54e6f0f14ed1f0c4c5d157aebe3057523ee64bef
Toby Corkindale <toby@dryft.net>
2011-05-09 15:33:49 +1000

    License and author info

--- script/scrape.pl
+++ script/scrape.pl
@@ -7,7 +7,13 @@ use WWW::Mechanize;
 use HTML::TreeBuilder::XPath;
 use XML::Simple;
 
-=pod
+=head1 NAME
+
+moreland_scrape.pl
+
+=head1 SYNOPSIS
+
+  moreland_scrape.pl > moreland.xml
 
 This script will print out the most-recent 15 planning permits advertised
 by Moreland council. As long as the script is run every day or few, this will
@@ -163,4 +169,26 @@ sub date_to_iso {
         return "$3-$2-$1"
     }
 }
+
+=head1 AUTHOR
+
+Toby Corkindale E<lt>tjc@cpan.orgE<gt>
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+=cut
 
